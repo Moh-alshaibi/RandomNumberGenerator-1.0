84f5c9c6751564be20ebd7a79ff82047fdfd2116
Moh-alshaibi <kib09005@gmail.com>
2023-01-20 21:24:28 +0100

    Added some logic

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,2 +1,12 @@
 'use strict';
-console.log('you have found an easter egg hehe')
+// class name = rNumber
+// id name = createNumber
+    // let randomNumber = Math.floor(Math.random()* 1000001)
+
+let x;
+
+
+document.getElementById("createNumber").onclick = function(){
+    x = Math.floor(Math.random()* 1000001)
+    document.getElementById("rNumber").innerHTML = x;
+}
