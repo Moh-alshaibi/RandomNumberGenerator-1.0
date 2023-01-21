1458a37d9f37e8baa38b8a1e944e67387f21d91d
Moh-alshaibi <kib09005@gmail.com>
2023-01-21 16:19:33 +0100

    Added all logic

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,12 @@
 'use strict';
-// class name = rNumber
-// id name = createNumber
-    // let randomNumber = Math.floor(Math.random()* 1000001)
 
 let x;
-
-
+let y;
 document.getElementById("createNumber").onclick = function(){
     x = Math.floor(Math.random()* 1000001)
     document.getElementById("rNumber").innerHTML = x;
+}
+document.getElementById("numberZero").onclick = function(){
+    y = 0
+    document.getElementById("rNumber").innerHTML = y;
 }
